7ccdd731a8e1b9285e103f4e0886a8facb6c65be
Alex Antonakakis <antonakakis1@gmail.com>
2021-12-09 14:40:57 +0200

    Added GUI's description

--- Intellij_Project_Template/src/view/GraphicsUI.java
+++ Intellij_Project_Template/src/view/GraphicsUI.java
@@ -1,4 +1,29 @@
 package view;
 
-public class GraphicsUI {
+import javax.swing.*;
+
+/**
+ * Payday GUI
+ * @version 1.0
+ * @author Alexandros Antonakakis (csd4802)
+ */
+
+public class GraphicsUI
+{
+    JFrame frame;
+    JPanel p1;
+    JPanel p2;
+    JMenu menu;
+    JMenuBar menu_bar;
+
+    /**
+     * Creates a new window and adds buttons and panels
+     * @type Constructor
+     */
+
+    public GraphicsUI()
+    {
+
+    }
+
 }
