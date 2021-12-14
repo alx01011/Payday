2f984369a1bbd9a84bd472e1fc393d1cb65ccbaa
Alex Antonakakis <antonakakis1@gmail.com>
2021-12-14 19:48:45 +0200

    Added javadoc comments

--- Intellij_Project_Template/src/model/special/ThursdayRiseInCrypto.java
+++ Intellij_Project_Template/src/model/special/ThursdayRiseInCrypto.java
@@ -1,7 +1,8 @@
 package model.special;
 
 
-import enums.Bet_Sports;
+import enums.Bet_Crypto;
+import model.dice.Dice;
 
 /**
  * ThursdayRiseInCrypto core
@@ -11,7 +12,20 @@ import enums.Bet_Sports;
 
 public class ThursdayRiseInCrypto {
 
-    private Bet_Sports choice;
+    private final Bet_Crypto choice;
+    private       Dice newRoll;
+
+    /**
+     * Constructor
+     * @param choice bet/no_bet
+     * @type Constructor
+     * @Postcondition A new object is created
+     */
+
+    public ThursdayRiseInCrypto(Bet_Crypto choice)
+    {
+        this.choice = choice;
+    }
 
 
 }
